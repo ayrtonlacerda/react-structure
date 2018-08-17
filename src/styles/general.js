986689ad268f54213ca830738057a43e34ed5735
Unknown <ayrtonlaceda01@gmail.com>
2018-08-17 17:00:30 -0300

    Terceirocomite
    estrutura e styles

---
+++ src/styles/general.js
@@ -0,0 +1,10 @@
+import colors from './colors';
+import metric from './metric';
+
+export default{
+  box: {
+    backgroundColor: colors.white,
+    borderRadius: metric.baseRadius,
+    padding: metric.basePadding,    
+  }
+};
---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import colors from './colors';
+import metric from './metrics';
+import general from './general';
+
+export { colors, metric, general };
